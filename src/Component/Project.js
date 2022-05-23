2ec3a0ad89b80c7d085b4750015ddd8de2b29e22
Wai Lok <wailokcheng020@gmail.com>
2022-05-23 12:30:25 +0100

    update project layout, picture, interactions

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -3,16 +3,17 @@ import { Row } from 'react-bootstrap';
 
 import ColOfProjectBlock from '../Helper/ColOfProjectBlock';
 
+// Image
+import project_1_snapshot from '../Image/project_1.png';
+
 class Project extends Component {
 
   render() {
+
     return (
       <div className={"project"}>
-        <Row>
-          <ColOfProjectBlock />
-          <ColOfProjectBlock />
-          <ColOfProjectBlock />
-          <ColOfProjectBlock />
+        <Row className={"info"}>
+          <ColOfProjectBlock title={"Calculator"} snapshot={project_1_snapshot} button_link={"https://lok020.github.io/calculator/"}/>
         </Row>
       </div>
     );
