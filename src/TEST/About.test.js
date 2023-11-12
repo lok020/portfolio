11fcdb458b33071ce807b2b3769f36132fedbb92
Wai Lok <wailokcheng020@gmail.com>
2023-11-12 11:45:58 +0000

    update title on home page

---
+++ src/TEST/About.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import About from '../Component/About';
+
+test('render About.js', () => {
+    // render(<About/>);
+    // const text = screen.getByText("Personal Information");
+    // expect(text).toBeInTheDocument();
+});
+
