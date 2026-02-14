d29f1584dfc0b3059257f8aa52d3f5b58213d550
wai-lok <wailokcheng020@gmail.com>
2026-02-14 18:08:34 +0000

    chore: remove unused code

--- src/TEST/About.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from '../Component/About';
-
-test('render About.js', () => {
-    // render(<About/>);
-    // const text = screen.getByText("Personal Information");
-    // expect(text).toBeInTheDocument();
-});
-
